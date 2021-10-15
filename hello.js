bb951d6b158df29e54129e28dc5b80fede2f56f5
Riaboshtan-Oleksandra <tstr.riaboshtan@gmail.com>
2021-10-15 14:49:33 +0300

    .

---
+++ hello.js
@@ -0,0 +1,3 @@
+//I didn't understand what kind of changes I needed to commit to pass 003 - Practical Tasks Assignment, so I just added this file to my repository
+
+console.log('Hello. This file added from first_branch)for_3d_lecture');
