3f4307c976d0d04a7f6a30108fc13776584b01e5
Riaboshtan-Oleksandra <92319246+Riaboshtan-Oleksandra@users.noreply.github.com>
2021-10-15 15:19:29 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 //I didn't understand what kind of changes I needed to commit to pass 003 - Practical Tasks Assignment, so I just added this file to my repository
 
 console.log('Hello. This file added from first_branch)for_3d_lecture');
-console.log('Changes added for second_try branch');
+console.log('This line is added from second_try branch');
