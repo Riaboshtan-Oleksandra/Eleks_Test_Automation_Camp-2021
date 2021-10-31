74fb485342aea443d01bae49e6412a80f0668e98
Riaboshtan-Oleksandra <tstr.riaboshtan@gmail.com>
2021-10-31 21:53:54 +0200

    deleted files with previous HWs

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //gather land vehils
+class Urban_Transport {
+  constructor(ownerName, ownerAge, vehicleType, fuelType, wheelsNumber) {
+    this.name = ownerName;
+    this.age = ownerAge;
+    this.type = vehicleType;
+    this.fuel = fuelType;
+    this.wheelsNumber = wheelsNumber;
+  }
+
+  description() {
+    console.log(`Name: ${this.name}`);
+  }
+
+  getDriverLicense() {
+    if (this.type == "bycicle") {
+      console.log();
+    }
+  }
+}
